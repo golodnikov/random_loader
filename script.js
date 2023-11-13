fe058f88be99813b5e241eb5f761e43feb37e469
golodnikov <golodnikovandrey56@gmail.com>
2023-11-13 11:51:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function loadImage() {
+    var loader = document.getElementById("loader");
+    var image = document.getElementById("dogImage");
+    loader.style.display = "block";
+    image.style.display = "none";
+  
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => {
+        loader.style.display = "none";
+        image.style.display = "block";
+        image.src = data.message;
+      })
+      .catch(error => console.error(error));
+  }
+  
+  setInterval(loadImage, 10000);
+  
